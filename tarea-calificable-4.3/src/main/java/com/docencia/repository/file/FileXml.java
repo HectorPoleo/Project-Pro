d3a5c8f1028a107165dd55dbb65bda3d6c0fd62e
HectorPoleo <hectorpoleo2007@gmail.com>
2026-03-16 15:17:24 +0000

    Se añaden tanto como FileCsv y FileXml

---
+++ tarea-calificable-4.3/src/main/java/com/docencia/repository/file/FileXml.java
@@ -0,0 +1,13 @@
+package com.docencia.repository.file;
+
+public class FileXml extends FileAbstract{
+    private static String path = "alumnos.xml";
+
+    public FileXml(){
+        super(path);
+    }
+
+    public FileXml(String path){
+        super(path);
+    }
+}
